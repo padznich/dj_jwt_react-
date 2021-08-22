ed53f7b7c7bbcf6c299b775efe92db5fb7cc1ccf
Pavel Slabko <slabkopg@gmail.com>
2021-08-22 11:27:51 +0300

    [ADD] docker-compose
    - web-dev
    - web-prod

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Django+React+JWT
 #### Setup
 
 
+```bash
+docker-compose up -d --build
+```
+
+
+# Services docs
+----
+
+
 [ Backend / Django ](./docs/be.md)
 ----
 
--- dj_djoser/api/v1/show/views.py
+++ dj_djoser/api/v1/show/views.py
@@ -1,16 +1,24 @@
-from rest_framework import status
-from rest_framework.generics import ListAPIView
+from rest_framework import status, serializers
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema
 
 
-class Show(ListAPIView):
+class ShowSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+
+
+class Show(GenericAPIView):
     permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        return ShowSerializer(many=True)
 
     @extend_schema()
     def get(self, *args, **kwargs):
-        return Response([
+        data = [
             {
               "id": 1,
               "name": "a",
@@ -23,6 +31,7 @@ class Show(ListAPIView):
               "id": 3,
               "name": "c",
             },
-        ],
-            status=status.HTTP_200_OK,
-        )
+        ]
+        s = ShowSerializer(data=data, many=True)
+        s.is_valid()
+        return Response(s.validated_data, status=status.HTTP_200_OK)
